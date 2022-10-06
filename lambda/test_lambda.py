7c02ad2352b07df7f1049e6bae1e0fe9ffc827c3
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2022-10-06 19:18:10 +0300

    Create test_lambda.py

---
+++ lambda/test_lambda.py
@@ -0,0 +1,11 @@
+x = lambda a, b: a * b
+print(x(int(input()), int(input())))
+
+# x = lambda first_text, second_text: first_text + second_text
+# print(x(input(), input()))
+
+
+# 1. Правят се променливи
+# 2. Прави се изчисление
+# 3. Принтира се името на променливата която държи lambda в случая 'x'
+# 4. Захранване на променливите
