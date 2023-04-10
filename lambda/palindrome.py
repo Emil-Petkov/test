afaadd777ed0ea1524f40a237ad46303529d53e3
hellhound <114022856+Emil-Petkov@users.noreply.github.com>
2023-04-10 22:48:33 +0300

    Delete palindrome.py

--- lambda/palindrome.py
+++
@@ -1,8 +0,0 @@
-def palindrome(text):
-    if text == text[::-1]:
-        return f"({text}) => This is a palindrome."
-    else:
-        return f"({text}) => Not a palindrome."
-
-
-print(palindrome(input("Еnter input and I'll tell you if it's a palindrome: ")))
