c0926d8d93ac608c8c838c7527cb75fe6f0ba0d6
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-06-15 18:45:55 +0300

    Update test_lambda.py

--- lambda/test_lambda.py
+++ lambda/test_lambda.py
@@ -23,3 +23,14 @@ print(list(filter(lambda x: x % 2 == 0, numbers))) # [2, 4, 6, 8, 10, 12, 14, 16
 # result = list(filter(check_numbers, numbers))
 
 # print(result)  # [2, 4, 6, 8, 10, 12, 14, 16]
+
+#####################################################################################
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+# def square_numbers(numbers):
+#     return list(map(lambda x: x ** 2, numbers))
+
+
+# print(square_numbers(numbers))  # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
