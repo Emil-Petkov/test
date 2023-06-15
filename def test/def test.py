293330902ebcb6ba655a991194fdbabd4fb5d56c
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-06-15 18:59:17 +0300

    Update def test.py

--- def test/def test.py
+++ def test/def test.py
@@ -3,6 +3,7 @@ numbers = [2, 33, 6, 123, 4, 85, 35, 1, 9, 3]
 
 def sorted_numbers(numbers):
     return list(reversed(sorted(numbers)))
+    # return list(map(lambda x: x, (reversed(sorted(numbers)))))
 
 
 print(sorted_numbers(numbers)) # [123, 85, 35, 33, 9, 6, 4, 3, 2, 1]
