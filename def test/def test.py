2885de5e3c16aa922fd98a8c01a64f824f82351d
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-06-15 19:36:46 +0300

    Update def test.py

--- def test/def test.py
+++ def test/def test.py
@@ -7,3 +7,15 @@ def sorted_numbers(numbers):
 
 
 print(sorted_numbers(numbers)) # [123, 85, 35, 33, 9, 6, 4, 3, 2, 1]
+
+
+###########################################################################
+
+def even_numbers(num):
+    return num % 2 == 0
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(list(filter(lambda x: even_numbers(x), numbers))) #  [2, 4, 6, 8, 10]
+
