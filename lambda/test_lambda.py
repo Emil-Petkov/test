4aa8c92c7e52017eea6cb47b903474be4f5fc65b
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-06-15 18:31:24 +0300

    Update test_lambda.py

--- lambda/test_lambda.py
+++ lambda/test_lambda.py
@@ -3,3 +3,23 @@ print(x(int(input()), int(input())))
 
 # x = lambda first, second: first + second
 # print(x(input(), input()))
+
+######################################################################################
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+print(list(filter(lambda x: x % 2 == 0, numbers))) # [2, 4, 6, 8, 10, 12, 14, 16]
+
+######################################################################################
+
+# def check_numbers(num):
+#     if num % 2 == 0:
+#         return True
+
+#     else:
+#         return False
+
+
+# result = list(filter(check_numbers, numbers))
+
+# print(result)  # [2, 4, 6, 8, 10, 12, 14, 16]
